4869cd8001c4cb49b9839ab692762ff905600336
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2022-02-22 14:35:51 +0200

    First dicegame version

--- useless_app/lib/peli.dart
+++ useless_app/lib/peli.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // ignore_for_file: deprecated_member_use, prefer_const_constructors, avoid_unnecessary_containers
@@ -11,13 +13,26 @@ class Peli extends StatefulWidget {
 
 class _PeliControlState extends State<Peli> {
   
+  static var listImages = [
+      "assets/dice_1.png",
+      "assets/dice_2.png",
+      "assets/dice_3.png",
+      "assets/dice_4.png",
+      "assets/dice_5.png",
+      "assets/dice_6.png"
+  ];
+
+  static var _random = Random();
+  var imageToShow = listImages[_random.nextInt(listImages.length)];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-          child: Center(
-            
+          margin: EdgeInsets.only(top: 50.0),
+          child: Center( 
+          child: Image.asset(imageToShow.toString(), width: 400, height: 446),
           )
         )
       ],
